71c8cb695d25f3206cc4b34ed8ab9ffae485fb5d
Sergio López <lopezs@ryanair.com>
2020-02-07 18:16:29 +0100

    Initial Commit

---
+++ iDwnld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDwnld
+//
+//  Created by Lopez de la Flor, Sergio on 07/02/2020.
+//  Copyright © 2020 Lopez de la Flor, Sergio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
